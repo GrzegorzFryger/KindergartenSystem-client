b256eda21ebfc0a5de3ef5e05025c94beb7154ed
Grzegorz <s12583@pjwstk.edu.pl>
2020-02-09 23:25:58 +0100

    Add structure to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {SharedModule} from './shared/shared.module';
+import {CoreModule} from './core/core.module';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    MatSliderModule,
+    BrowserAnimationsModule, FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
