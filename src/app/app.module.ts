ba87b75673d76705494c947c75fc70e92cffd48e
Grzegorz <s12583@pjwstk.edu.pl>
2020-02-10 01:12:33 +0100

    Update css structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatSliderModule} from '@angular/material/slider';
 import {SharedModule} from './shared/shared.module';
 import {CoreModule} from './core/core.module';
-import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { NavbarComponent } from './layout/navbar/navbar.component';
+import {HeaderComponent} from './layout/header/header.component';
+import {FooterComponent} from './layout/footer/footer.component';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatToolbar, MatToolbarModule} from '@angular/material/toolbar';
+import {MatButton, MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
@@ -18,16 +19,19 @@ import {FlexLayoutModule} from '@angular/flex-layout';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavbarComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CoreModule,
     MatSliderModule,
+    MatToolbarModule,
+    MatButtonModule,
     BrowserAnimationsModule, FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
