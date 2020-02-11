241b8ee72365d26d99a6257e9b2388ca56bebed4
Grzegorz <s12583@pjwstk.edu.pl>
2020-02-11 15:20:42 +0100

    Update project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./core/core.module').then(m => m.CoreModule)
+  }
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatSliderModule} from '@angular/material/slider';
-import {SharedModule} from './shared/shared.module';
-import {CoreModule} from './core/core.module';
 import {HeaderComponent} from './layout/header/header.component';
 import {FooterComponent} from './layout/footer/footer.component';
 import {FlexLayoutModule} from '@angular/flex-layout';
@@ -19,12 +16,10 @@ import {MatButton, MatButtonModule} from '@angular/material/button';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule,
     MatSliderModule,
     MatToolbarModule,
     MatButtonModule,
