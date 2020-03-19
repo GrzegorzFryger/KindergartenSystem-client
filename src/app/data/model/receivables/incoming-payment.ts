54918d59fa2fde8e4250997f3ae9b9ef04c2e8f0
Sebastian Kaminski <skaminskirepo@gmail.com>
2020-03-19 23:10:33 +0100

    Add components for finances & receivables modules

--- src/app/data/model/receivables/incoming-payment.ts
+++ src/app/data/model/receivables/incoming-payment.ts
@@ -1,2 +1,10 @@
 export class IncomingPayment {
+  paymentType: string;
+  transactionDate: string;
+  contractorDetails: string;
+  title: string;
+  transactionAmount: number;
+  transactionCurrency: string;
+  childId: string;
+  guardianId: string;
 }
