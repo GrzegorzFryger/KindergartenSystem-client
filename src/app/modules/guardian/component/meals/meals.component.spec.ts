e42b14f18ec1c9d88fe525d551a073a30eaaf096
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-03-19 22:30:40 +0100

    Resolve conflict in meal component spec

--- src/app/modules/guardian/component/meals/meals.component.spec.ts
+++ src/app/modules/guardian/component/meals/meals.component.spec.ts
@@ -1,10 +1,8 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-<<<<<<< HEAD:src/app/modules/guardian/component/meals/meals.component.spec.ts
+
 import { MealsComponent } from './meals.component';
-=======
-import {FooterComponent} from './footer.component';
->>>>>>> 53df3ee5ac24fb9a6db8cd4fa15b3e56f920bd7a:src/app/layout/footer/footer.component.spec.ts
+
 
 describe('MealsComponent', () => {
   let component: MealsComponent;
@@ -12,11 +10,7 @@ describe('MealsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-<<<<<<< HEAD:src/app/modules/guardian/component/meals/meals.component.spec.ts
       declarations: [ MealsComponent ]
-=======
-      declarations: [FooterComponent]
->>>>>>> 53df3ee5ac24fb9a6db8cd4fa15b3e56f920bd7a:src/app/layout/footer/footer.component.spec.ts
     })
       .compileComponents();
   }));
