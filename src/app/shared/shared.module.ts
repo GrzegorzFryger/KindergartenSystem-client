7996363269cd5e13d8ae8fe71cc776fae070bf07
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-03-20 17:21:02 +0100

    Add routerLink do meal module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NavbarComponent} from './navbar/navbar.component';
 import {MatButtonModule} from '@angular/material/button';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [
-    CommonModule,
-    MatButtonModule
-  ],
+    imports: [
+        CommonModule,
+        MatButtonModule,
+        RouterModule
+    ],
   exports: [NavbarComponent]
 })
 export class SharedModule {
