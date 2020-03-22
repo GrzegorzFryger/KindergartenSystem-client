7364900777eff777465ef3a93265400f9259a486
Sebastian Kaminski <skaminskirepo@gmail.com>
2020-03-22 20:41:20 +0100

    Add admin components for receivables-module

--- src/app/modules/administrator/administrator.module.ts
+++ src/app/modules/administrator/administrator.module.ts
@@ -3,13 +3,17 @@ import {CommonModule} from '@angular/common';
 
 import {AdministratorRoutingModule} from './administrator-routing.module';
 import {AdministratorComponent} from './administrator.component';
+import {MatCardModule} from '@angular/material/card';
+import { CashPaymentsComponent } from './receivables/cash-payments/cash-payments.component';
+import { TransactionsComponent } from './receivables/transactions/transactions.component';
 
 
 @NgModule({
-  declarations: [AdministratorComponent],
+  declarations: [AdministratorComponent, CashPaymentsComponent, TransactionsComponent],
   imports: [
     CommonModule,
-    AdministratorRoutingModule
+    AdministratorRoutingModule,
+    MatCardModule
   ]
 })
 export class AdministratorModule { }
