213685a4420d03222e165678f770547c27398ff7
Sebastian Kaminski <skaminskirepo@gmail.com>
2020-03-22 12:44:14 +0100

    Add possibility to retrieve receivables

--- src/app/modules/guardian/page/receiables/receiables.component.ts
+++ src/app/modules/guardian/page/receiables/receiables.component.ts
@@ -1,4 +1,9 @@
+import { IncomingPaymentsService } from './../../../../data/service/receivables/incoming-payments.service';
+import { SnackErrorHandlingService } from 'src/app/core/snack-error-handling/snack-error-handling.service';
 import { Component, OnInit } from '@angular/core';
+import { IncomingPayment } from 'src/app/data/model/receivables/incoming-payment';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-receiables',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./receiables.component.scss']
 })
 export class ReceiablesComponent implements OnInit {
+  public incomingPaymentsForAllChildren: Observable<Array<IncomingPayment>>;
 
-  constructor() { }
+  constructor(private incomingPaymentsService: IncomingPaymentsService,
+              private snackErrorHandlingService: SnackErrorHandlingService) { }
 
   ngOnInit(): void {
+    this.incomingPaymentsForAllChildren = this.incomingPaymentsService
+    .getAllIncomingPaymentsForGuardian('c4029244-e8ff-4328-8658-28964dda3c4e')
+    .pipe(
+      catchError(err => {
+        this.snackErrorHandlingService.openSnackBar();
+        return throwError(err);
+      }),
+      map(response => {
+        console.log(response);
+        return response;
+      })
+    );
   }
 
 }
