fc5c1695dffe1a77acfafd5bfd197069e755c228
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-03-23 10:18:01 +0100

    Resolve merge conflicts

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,20 +2,10 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NavbarComponent} from './layout/navbar/navbar.component';
 import {MatButtonModule} from '@angular/material/button';
-<<<<<<< HEAD
 import {RouterModule} from '@angular/router';
-
-
-@NgModule({
-  declarations: [NavbarComponent],
-    imports: [
-        CommonModule,
-        MatButtonModule,
-        RouterModule
-    ],
-  exports: [NavbarComponent]
-=======
 import { UserComponent } from './user/user.component';
+
+
 
 @NgModule({
   declarations: [NavbarComponent, UserComponent],
@@ -24,7 +14,6 @@ import { UserComponent } from './user/user.component';
     MatButtonModule
   ],
   exports: [NavbarComponent, UserComponent]
->>>>>>> e8b98a2c14df437178355d5c661077c0ac5bcf75
 })
 export class SharedModule {
 }
