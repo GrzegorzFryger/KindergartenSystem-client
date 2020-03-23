bd00f14e1a5d23dee7596aa32ce3d069c7dbeb47
Grzegorz <s12583@pjwstk.edu.pl>
2020-03-23 23:48:05 +0100

    Add administrator layout
    Took 10 minutes

--- src/app/modules/administrator/administrator.module.ts
+++ src/app/modules/administrator/administrator.module.ts
@@ -4,16 +4,18 @@ import {CommonModule} from '@angular/common';
 import {AdministratorRoutingModule} from './administrator-routing.module';
 import {AdministratorComponent} from './administrator.component';
 import {MatCardModule} from '@angular/material/card';
-import { CashPaymentsComponent } from './receivables/cash-payments/cash-payments.component';
-import { TransactionsComponent } from './receivables/transactions/transactions.component';
+import {CashPaymentsComponent} from './receivables/cash-payments/cash-payments.component';
+import {TransactionsComponent} from './receivables/transactions/transactions.component';
+import {SharedModule} from '../../shared/shared.module';
 
 
 @NgModule({
   declarations: [AdministratorComponent, CashPaymentsComponent, TransactionsComponent],
-  imports: [
-    CommonModule,
-    AdministratorRoutingModule,
-    MatCardModule
-  ]
+    imports: [
+        CommonModule,
+        AdministratorRoutingModule,
+        MatCardModule,
+        SharedModule
+    ]
 })
 export class AdministratorModule { }
