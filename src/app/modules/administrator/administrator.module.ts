5cf63b87607303c2fb3425e1f077881c404a3f6f
Grzegorz <s12583@pjwstk.edu.pl>
2020-03-24 00:05:37 +0100

    Add routing for receivables
    Took 17 minutes

--- src/app/modules/administrator/administrator.module.ts
+++ src/app/modules/administrator/administrator.module.ts
@@ -7,10 +7,11 @@ import {MatCardModule} from '@angular/material/card';
 import {CashPaymentsComponent} from './receivables/cash-payments/cash-payments.component';
 import {TransactionsComponent} from './receivables/transactions/transactions.component';
 import {SharedModule} from '../../shared/shared.module';
+import {ReceivablesComponent} from './receivables/receivables.component';
 
 
 @NgModule({
-  declarations: [AdministratorComponent, CashPaymentsComponent, TransactionsComponent],
+  declarations: [AdministratorComponent, CashPaymentsComponent, TransactionsComponent, ReceivablesComponent],
     imports: [
         CommonModule,
         AdministratorRoutingModule,
