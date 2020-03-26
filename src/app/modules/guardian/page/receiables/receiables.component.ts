32676a6eb9cb414cbb2f7e63414d7e3f00e03d3a
Grzegorz <s12583@pjwstk.edu.pl>
2020-03-26 23:16:59 +0100

    Refactor code
    Took 21 minutes

--- src/app/modules/guardian/page/receiables/receiables.component.ts
+++ src/app/modules/guardian/page/receiables/receiables.component.ts
@@ -1,14 +1,13 @@
-import { MatTableDataSource } from '@angular/material/table';
-import { IncomingPaymentsService } from './../../../../data/service/receivables/incoming-payments.service';
-import { SnackErrorHandlingService } from 'src/app/core/snack-error-handling/snack-error-handling.service';
-import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
-import { IncomingPayment } from 'src/app/data/model/receivables/incoming-payment';
-import { Observable, throwError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { UserService } from 'src/app/data/service/users/user.service';
-import { User } from 'src/app/data/model/users/user';
+import {MatTableDataSource} from '@angular/material/table';
+import {IncomingPaymentsService} from './../../../../data/service/receivables/incoming-payments.service';
+import {SnackErrorHandlingService} from 'src/app/core/snack-error-handling/snack-error-handling.service';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {IncomingPayment} from 'src/app/data/model/receivables/incoming-payment';
+import {Observable, throwError} from 'rxjs';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {UserService} from 'src/app/data/service/users/user.service';
+import {catchError} from 'rxjs/operators';
 
 const ERROR_MESSAGE = 'error';
 
@@ -22,55 +21,35 @@ export class ReceiablesComponent implements OnInit, AfterViewInit {
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   public dataSource: MatTableDataSource<IncomingPayment> = new MatTableDataSource();
+
   public columnsToDisplay: string[] = ['contractorDetails', 'paymentType', 'title', 'transactionAmount', 'transactionDate'];
   public incomingPaymentsForAllChildren: Observable<Array<IncomingPayment>>;
 
   constructor(private incomingPaymentsService: IncomingPaymentsService,
               private userService: UserService,
-              private snackErrorHandlingService: SnackErrorHandlingService) { }
+              private snackErrorHandlingService: SnackErrorHandlingService) {
+  }
 
   ngOnInit(): void {
-    this.userService.currentUser.subscribe(u => {
-      this.getIncomingPayments(u);
+    this.userService.currentUser.subscribe(user => {
+      this.incomingPaymentsService.getAllIncomingPaymentsForGuardian(user.id).subscribe(resp => {
+        this.setUpDataTable(resp);
+      }, catchError(err => {
+        this.snackErrorHandlingService.openSnackBar('can not find..');
+        return throwError(err);
+      }));
     });
-    this.initializeDataSorting();
   }
 
   ngAfterViewInit(): void {
-    this.initializeData();
-    this.initializePaginator();
   }
 
-  private getIncomingPayments(u: User) {
-    this.incomingPaymentsForAllChildren = this.incomingPaymentsService
-    .getAllIncomingPaymentsForGuardian(u.id)
-    .pipe(
-      catchError(err => {
-        this.snackErrorHandlingService.openSnackBar(ERROR_MESSAGE);
-        return throwError(err);
-      }),
-      map(response => {
-        console.log(response);
-        return response;
-      })
-    );
-  }
-
-  private initializeDataSorting(): void {
+  private setUpDataTable(incomingPayment: Array<IncomingPayment>): void {
+    this.dataSource.data = incomingPayment;
     this.dataSource.sort = this.sort;
-  }
-
-  private initializeData(): void {
-    this.incomingPaymentsForAllChildren.subscribe(
-      payments => {
-        this.dataSource.data = payments;
-      }
-    );
-  }
-
-  private initializePaginator(): void {
     this.dataSource.paginator = this.paginator;
     this.dataSource.paginator._intl.itemsPerPageLabel = 'Ilość rekordów na stronę'; // TODO Change it into better solution (more global)
   }
+
 
 }
