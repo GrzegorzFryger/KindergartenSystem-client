fd2673f5571454688f1a16d22ea6d95ce5780357
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-03-30 18:18:29 +0200

    Fix batch script

--- cert/make-cert.bat
+++ cert/make-cert.bat
@@ -1,8 +1,7 @@
 @echo off
 
-cd cert
 
-openssl req -new -x509 -newkey rsa:2048 -sha256 -nodes -keyout localhost.key -days 30 out localhost.crt -config certificate.cnf
+openssl req -new -x509 -newkey rsa:2048 -sha256 -nodes -keyout localhost.key -days 356 -out localhost.crt -config certificate.cnf
 
 echo.
 echo -----
