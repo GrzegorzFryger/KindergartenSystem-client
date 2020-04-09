e456aafd2e7e4e06e3bbef916402092afb13887c
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-04-09 18:19:47 +0200

    Add service status component

---
+++ src/app/modules/administrator/actuator/services-status/services-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-services-status',
+  templateUrl: './services-status.component.html',
+  styleUrls: ['./services-status.component.scss']
+})
+export class ServicesStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
