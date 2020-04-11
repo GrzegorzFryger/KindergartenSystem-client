4d09ae09bffbe4c2646336edc992fe51b529ab40
Adam <s15814@pjwstk.edu.pl>
2020-04-11 19:36:11 +0200

    Add possibility to add a new day off

--- src/app/modules/administrator/page/calendar/day-off-work/day-off-work.component.ts
+++ src/app/modules/administrator/page/calendar/day-off-work/day-off-work.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DayOffWorkService} from '../../../../../data/service/absence/day-off-work.service';
+import {DayOffWork, EventType} from '../../../../../data/model/absence/day-off-work';
 
 @Component({
   selector: 'app-day-off-work',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DayOffWorkComponent implements OnInit {
 
-  constructor() { }
+  dayOffWork: DayOffWork;
+  // enumValues = [
+  //   {display: 'Święto', value: 'HOLIDAY'},
+  //   {display: 'Weekend', value: 'WEEKEND'},
+  //   {display: 'Wydarzenie wewnętrzne', value: 'INTERNAL_EVENT'}
+  // ];
+
+  constructor(private dayOffWorkService: DayOffWorkService) {
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(submittedForm) {
+    this.dayOffWork = new DayOffWork();
+    this.dayOffWork.date = submittedForm.value.date;
+    this.dayOffWork.name = submittedForm.value.name;
+    this.dayOffWork.eventType = submittedForm.value.eventType;
+    this.dayOffWorkService.createDayOffWork(this.dayOffWork).subscribe(resp =>
+      console.log(resp));
+    submittedForm.reset();
   }
 
 }
