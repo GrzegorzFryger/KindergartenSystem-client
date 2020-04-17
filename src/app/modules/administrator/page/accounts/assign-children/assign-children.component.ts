4139d3c3db9a42e6690e03fa2ee7fa8c5d01495c
Grzegorz <s12583@pjwstk.edu.pl>
2020-04-17 23:16:55 +0200

    Update assign children
    pass selected values from children list
    
    Took 41 seconds

--- src/app/modules/administrator/page/accounts/assign-children/assign-children.component.ts
+++ src/app/modules/administrator/page/accounts/assign-children/assign-children.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {FormGroup} from '@angular/forms';
+import {Child} from '../../../../../data/model/accounts/child';
 
 @Component({
   selector: 'app-assign-children',
@@ -8,13 +9,22 @@ import {FormGroup} from '@angular/forms';
   encapsulation: ViewEncapsulation.None
 })
 export class AssignChildrenComponent implements OnInit {
+  childListMode: string;
+
+  private selectedChildren: Array<Child>;
   isLinear: boolean;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
 
-  constructor() { }
+  constructor() {
+    this.childListMode = 'read';
+  }
 
   ngOnInit(): void {
   }
 
+  onSelectedChildren(event: Array<Child>) {
+    this.selectedChildren = event;
+    console.log(event);
+  }
 }
