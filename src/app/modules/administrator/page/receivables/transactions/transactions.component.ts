ed268e9cad0d20ecf199e6d45de62f8a542df382
Grzegorz <s12583@pjwstk.edu.pl>
2020-04-22 00:40:17 +0200

    Fix and improvements
    Took 1 hour 0 minutes

--- src/app/modules/administrator/page/receivables/transactions/transactions.component.ts
+++ src/app/modules/administrator/page/receivables/transactions/transactions.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {NavigationEnd, Router} from '@angular/router';
 import {MatDialog} from '@angular/material/dialog';
 import {ImportComponent} from './import/import.component';
+import {Observable, ReplaySubject} from 'rxjs';
+import {Transaction} from '../../../../../data/model/receivables/transaction';
+import {TransactionsService} from '../../../../../data/service/receivables/transactions.service';
 
 @Component({
   selector: 'app-transactions',
@@ -9,18 +12,28 @@ import {ImportComponent} from './import/import.component';
   styleUrls: ['./transactions.component.scss']
 })
 export class TransactionsComponent implements OnInit {
-
   public isSelected: boolean;
+  public transactionColumnsToDisplay: string[] = ['transactionDate', 'contractorDetails', 'title', 'transactionAmount'];
+  public transactionOutput: { transactions: Observable<Array<Transaction>>, columnToDisplay: Array<string> };
+  private transactionSub: ReplaySubject<Array<Transaction>>;
 
   constructor(private router: Router,
-              public dialog: MatDialog) {
+              public dialog: MatDialog,
+              private transactionsService: TransactionsService
+  ) {
     this.isSelected = true;
+    this.transactionSub = new ReplaySubject<Array<Transaction>>();
+    this.transactionOutput = {transactions: this.transactionSub.asObservable(), columnToDisplay: this.transactionColumnsToDisplay};
   }
 
   ngOnInit(): void {
     this.router.events.subscribe(ev => {
       const navEnf = ev as NavigationEnd;
       this.isSelected = navEnf.url === '/administrator/transactions';
+    });
+
+    this.transactionsService.getAllTransactionsFromPastMonth().subscribe(trans => {
+      this.transactionSub.next(trans);
     });
   }
 
