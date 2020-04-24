765017d9554a75853c7739c52c610f3c8826c651
Adam <s15814@pjwstk.edu.pl>
2020-04-24 21:41:46 +0200

    Implement adding new absences

--- src/app/modules/administrator/page/calendar/absence/add-absence/add-absence.component.ts
+++ src/app/modules/administrator/page/calendar/absence/add-absence/add-absence.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Child} from '../../../../../../data/model/accounts/child';
 import {ChildService} from '../../../../../../data/service/accounts/child.service';
+import {Absence} from '../../../../../../data/model/absence/absence';
 import {AbsenceService} from '../../../../../../data/service/absence/absence.service';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
@@ -19,9 +20,15 @@ export class AddAbsenceComponent implements OnInit {
 
   dataSourceChild: MatTableDataSource<Child> = new MatTableDataSource();
 
-  public columnsToDisplay: string[] = ['name', 'surname', 'pesel'];
+  public columnsToDisplay: string[] = ['name', 'surname', 'pesel', 'selected'];
 
   private children: Observable<Array<Child>>;
+
+  absenceToAdd: Absence;
+  selectedChildId: string;
+  reason: string;
+  dateFrom: Date = null;
+  dateTo: Date = null;
 
   constructor(private childService: ChildService, private absenceService: AbsenceService) {
     this.children = this.childService.getAllChildren();
@@ -42,7 +49,25 @@ export class AddAbsenceComponent implements OnInit {
   }
 
   onSubmit(submittedForm) {
+    this.dateFrom = submittedForm.value.dateFrom;
+    this.dateTo = submittedForm.value.dateTo;
+    this.reason = submittedForm.value.reason;
 
+    if (this.dateTo == null && this.dateFrom != null) {
+      this.absenceToAdd = new Absence();
+      this.absenceToAdd.date = this.dateFrom;
+      this.absenceToAdd.childId = this.selectedChildId;
+      this.absenceToAdd.reason = this.reason;
+      this.absenceService.createAbsence(this.absenceToAdd).subscribe(resp => {
+        console.log(resp);
+      });
+    }
+    // TODO: implement adding absence range
+    submittedForm.reset();
+  }
+
+  getChildIdOnClick(childId: string): void {
+    this.selectedChildId = childId;
   }
 
 }
