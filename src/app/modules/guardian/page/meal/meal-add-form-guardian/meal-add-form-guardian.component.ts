c2624c82fe196c6cb42c91fb8b1a0dca40526fd5
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-04-26 11:08:10 +0200

    Init add meal form for parent

---
+++ src/app/modules/guardian/page/meal/meal-add-form-guardian/meal-add-form-guardian.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meal-add-form-guardian',
+  templateUrl: './meal-add-form-guardian.component.html',
+  styleUrls: ['./meal-add-form-guardian.component.scss']
+})
+export class MealAddFormGuardianComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
