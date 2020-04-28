00ccebd34d4bc4e10670f35127d843fe9559baff
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-04-28 20:52:43 +0200

    Implement logouy method

--- src/app/shared/user/user.component.ts
+++ src/app/shared/user/user.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {AccountService} from '../../data/service/accounts/account.service';
 import {Observable} from 'rxjs';
 import {Account} from '../../data/model/accounts/account';
+import {environment} from '../../core/environment.dev';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -12,11 +14,16 @@ export class UserComponent implements OnInit {
 
   public user: Observable<Account>;
 
-  constructor(private userService: AccountService) {
+  constructor(private userService: AccountService,
+              private router: Router) {
   }
 
   ngOnInit(): void {
     this.user = this.userService.currentUser;
   }
 
+  logout() {
+    localStorage.clear();
+    this.router.navigate([environment.routes.signInUrl]);
+  }
 }
