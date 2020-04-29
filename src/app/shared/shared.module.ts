7aec9b9fd0fcc07446b8b4074c38ed4dbfcf31c7
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-04-29 11:19:59 +0200

    Add Buttons to change user context

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,17 +5,21 @@ import {RouterModule} from '@angular/router';
 import {UserComponent} from './user/user.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [UserComponent],
-    imports: [
-        CommonModule,
-        MatButtonModule,
-        RouterModule,
-        MatCardModule,
-        MatMenuModule
-    ],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    RouterModule,
+    MatCardModule,
+    MatMenuModule,
+    MatButtonToggleModule,
+    MatIconModule
+  ],
   exports: [UserComponent]
 })
 export class SharedModule {
