5db1ee2aaf90d44690015a56589940b54e6c9b24
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-04-29 12:50:36 +0200

    Add presentation which role is selected

--- src/app/shared/user/user.component.ts
+++ src/app/shared/user/user.component.ts
@@ -26,6 +26,7 @@ export class UserComponent implements OnInit {
   ngOnInit(): void {
     this.user = this.userService.currentUser;
     this.identifyRoles();
+
   }
 
   logout(): void {
@@ -33,10 +34,20 @@ export class UserComponent implements OnInit {
     this.router.navigate([environment.routes.signInUrl]);
   }
 
+  showSelectedRole(selected: string): void {
+    if (localStorage.getItem('selectedRole') == null) {
+      this.selectedRole = selected;
+    } else {
+      this.selectedRole = localStorage.getItem('selectedRole');
+    }
+
+  }
+
   identifyRoles(): void {
     this.user.subscribe(user => {
       const roles = user.roles;
 
+      this.showSelectedRole(roles[0].name);
 
       roles.forEach(u => {
         if (u.name === 'ADMINISTRATOR') {
@@ -53,7 +64,7 @@ export class UserComponent implements OnInit {
   }
 
   changeRole(role: string): void {
-    this.selectedRole = role;
+    localStorage.setItem('selectedRole', role);
     if (role === 'ADMINISTRATOR') {
       this.router.navigate([environment.routes.homeUrlAdmin]);
     }
