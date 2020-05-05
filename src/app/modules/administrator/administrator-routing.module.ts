84a2293be080e36519ab13e9a90e820cb63921bb
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2020-05-05 21:38:09 +0200

    Add datapicker to order meal

--- src/app/modules/administrator/administrator-routing.module.ts
+++ src/app/modules/administrator/administrator-routing.module.ts
@@ -64,12 +64,10 @@ const routes: Routes = [
       {
         path: 'meal',
         component: MealComponent,
-        children: [
-          {
-            path: 'order',
-            component: MealOrderComponent
-          }
-        ]
+      },
+      {
+        path: 'order',
+        component: MealOrderComponent,
       },
       {
         path: 'dayOff',
--- src/app/modules/administrator/page/meal/meal-order/meal-order.component.ts
+++ src/app/modules/administrator/page/meal/meal-order/meal-order.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatCalendar, MatCalendarCellCssClasses} from '@angular/material/datepicker';
+import {DayOffWork} from '../../../../../data/model/absence/day-off-work';
+import {Absence} from '../../../../../data/model/absence/absence';
+import {Observable} from 'rxjs';
+import {Child} from '../../../../../data/model/accounts/child';
+import {DayOffWorkService} from '../../../../../data/service/absence/day-off-work.service';
+import {AbsenceService} from '../../../../../data/service/absence/absence.service';
+import {SelectedChildService} from '../../../../guardian/component/children/selected-child.service';
+import {MatDialog} from '@angular/material/dialog';
+import {SnackMessageHandlingService} from '../../../../../core/snack-message-handling/snack-message-handling.service';
+
 
 @Component({
   selector: 'app-meal-order',
@@ -6,10 +17,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meal-order.component.scss']
 })
 export class MealOrderComponent implements OnInit {
+  @ViewChild('calendar')
+  myCalendar: MatCalendar<any>;
+  selectedDate: any;
+  maxDate: Date = new Date();
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor() {
+
   }
 
+
+  ngOnInit(): void {
+
+  }
+
+
 }
+
