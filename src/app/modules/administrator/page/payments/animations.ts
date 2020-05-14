91a32e9474d188685f2238c8a362188761174461
Grzegorz <s12583@pjwstk.edu.pl>
2020-05-14 22:11:06 +0200

    Refactor animations for slide left
    Took 1 hour 10 minutes

--- src/app/modules/administrator/page/payments/animations.ts
+++ src/app/modules/administrator/page/payments/animations.ts
@@ -1,22 +1,33 @@
 import {animate, state, style, transition, trigger} from '@angular/animations';
 
-export const fadeAnimation = trigger('move', [
+export const fadeAnimation2 = trigger('paymentTable', [
   state('initial', style({
-    display: 'none',
+    width: '100%'
   })),
   state('final', style({
-    transform: 'translate3d(0, 0, 0px)',
-  })),
-  transition('initial=>final', animate('500ms 500ms cubic-bezier(0.35, 0, 0.25, 1)')),
-  transition('final=>initial', animate('500ms cubic-bezier(0.35, 0, 0.25, 1)'))
-]);
-
-export const fadeAnimation2 = trigger('move2', [
-  state('initial', style({})),
-  state('final', style({
-    transform: 'translate3d(0, -100%, 0px)',
-    display: 'none'
+    width: '30%'
   })),
   transition('initial=>final', animate('500ms cubic-bezier(0.35, 0, 0.25, 1)')),
   transition('final=>initial', animate('500ms cubic-bezier(0.35, 0, 0.25, 1)'))
 ]);
+
+export const fadeAnimation = trigger('paymentChildComponent', [
+  state('initial', style({
+  })),
+  state('final', style({
+    width: '70%'
+  })),
+  transition('initial=>final', animate('500ms cubic-bezier(0.35, 0, 0.25, 1)')),
+  transition('final=>initial', animate('500ms cubic-bezier(0.35, 0, 0.25, 1)'))
+]);
+
+export const showHide = trigger('hide', [
+  state('initial', style({
+    display: 'none'
+  })),
+  state('final', style({
+  })),
+  transition('initial=>final', animate('0ms')),
+  transition('final=>initial', animate('0ms'))
+]);
+
