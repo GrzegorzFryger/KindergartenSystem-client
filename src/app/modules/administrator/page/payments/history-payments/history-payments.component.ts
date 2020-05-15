44658fc4d31207ee6eef617610174690ebbacc7f
Grzegorz <s12583@pjwstk.edu.pl>
2020-05-15 11:49:56 +0200

    Add history payments
    Took 19 seconds

---
+++ src/app/modules/administrator/page/payments/history-payments/history-payments.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-history-payments',
+  templateUrl: './history-payments.component.html',
+  styleUrls: ['./history-payments.component.scss']
+})
+export class HistoryPaymentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
