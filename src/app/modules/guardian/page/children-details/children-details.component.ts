fe91b79cf6fe14dd134265ca93ca3ca9eb276433
Adam <s15814@pjwstk.edu.pl>
2020-05-22 16:44:07 +0200

    Add table with absences for selected child

--- src/app/modules/guardian/page/children-details/children-details.component.ts
+++ src/app/modules/guardian/page/children-details/children-details.component.ts
@@ -1,8 +1,12 @@
-import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Component, OnInit, OnDestroy, ViewChildren, QueryList} from '@angular/core';
 import {AbsenceService} from '../../../../data/service/absence/absence.service';
 import {SelectedChildService} from '../../component/children/selected-child.service';
 import {Observable, Subscription} from 'rxjs';
 import {Child} from '../../../../data/model/accounts/child';
+import {Absence} from '../../../../data/model/absence/absence';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-children-details',
@@ -11,8 +15,16 @@ import {Child} from '../../../../data/model/accounts/child';
 })
 export class ChildrenDetailsComponent implements OnInit, OnDestroy {
 
+  @ViewChildren(MatPaginator) paginator = new QueryList<MatPaginator>();
+  @ViewChildren(MatSort) sort = new QueryList<MatSort>();
+
+  public absenceDataSource: MatTableDataSource<Absence> = new MatTableDataSource();
+
+  public columnsToDisplay: string[] = ['date', 'reason'];
+
   selectedChild: Observable<Child>;
   selectedChildId: string;
+  selectedChildName: string;
 
   private childSubscription: Subscription;
 
@@ -24,6 +36,12 @@ export class ChildrenDetailsComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.childSubscription = this.selectedChild.subscribe(child => {
       this.selectedChildId = child.id;
+      this.selectedChildName = child.name + ' ' + child.surname;
+      this.absenceService.getAllAbsencesByChildId(this.selectedChildId).subscribe(absences => {
+        this.absenceDataSource.data = absences;
+        this.absenceDataSource.sort = this.sort.toArray()[0];
+        this.absenceDataSource.paginator = this.paginator.toArray()[0];
+      });
     });
   }
 
