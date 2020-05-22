91d22e014666c524a5374af3d1daf6b8adc40a2c
Adam <s15814@pjwstk.edu.pl>
2020-05-22 15:30:40 +0200

    Add ability to get id of selected child

--- src/app/modules/guardian/page/children-details/children-details.component.ts
+++ src/app/modules/guardian/page/children-details/children-details.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {AbsenceService} from '../../../../data/service/absence/absence.service';
+import {SelectedChildService} from '../../component/children/selected-child.service';
+import {Observable, Subscription} from 'rxjs';
+import {Child} from '../../../../data/model/accounts/child';
 
 @Component({
   selector: 'app-children-details',
   templateUrl: './children-details.component.html',
   styleUrls: ['./children-details.component.scss']
 })
-export class ChildrenDetailsComponent implements OnInit {
+export class ChildrenDetailsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  selectedChild: Observable<Child>;
+  selectedChildId: string;
+
+  private childSubscription: Subscription;
+
+  constructor(private absenceService: AbsenceService,
+              private selectedChildService: SelectedChildService) {
+    this.selectedChild = selectedChildService.selectedChild;
+  }
 
   ngOnInit(): void {
+    this.childSubscription = this.selectedChild.subscribe(child => {
+      this.selectedChildId = child.id;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.childSubscription.unsubscribe();
   }
 
 }
