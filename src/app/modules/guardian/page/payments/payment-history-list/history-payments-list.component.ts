3d5dea88480d0e996d463662cd10b4b4bfd24904
Grzegorz <s12583@pjwstk.edu.pl>
2020-05-25 00:03:09 +0200

    Add payment history to guardian
    Took 1 hour 42 minutes

---
+++ src/app/modules/guardian/page/payments/payment-history-list/history-payments-list.component.ts
@@ -0,0 +1,50 @@
+import {Component, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {Observable, Subscription} from 'rxjs';
+import {MatTableDataSource} from '@angular/material/table';
+import {PaymentHistory} from '../../../../../data/model/payments/payment-history';
+
+@Component({
+  selector: 'app-history-payments-list',
+  templateUrl: './history-payments-list.component.html',
+  styleUrls: ['./history-payments-list.component.scss']
+})
+export class HistoryPaymentsListComponent implements OnInit, OnDestroy {
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @Input() dataSource: {
+    data: Observable<Array<PaymentHistory>>, columnToDisplay: Observable<Array<string>>,
+  };
+  public dataSourceToDisplay: MatTableDataSource<PaymentHistory>;
+  public columnsToDisplay: string[];
+  private dataSub: Subscription;
+
+  constructor() {
+    this.dataSourceToDisplay = new MatTableDataSource();
+  }
+
+  ngOnInit(): void {
+    this.dataSub = this.dataSource.data.subscribe(payment => {
+      if (payment) {
+        this.dataSourceToDisplay.data = payment;
+        this.dataSource.columnToDisplay.subscribe(col => {
+          this.columnsToDisplay = col;
+        });
+        this.dataSourceToDisplay.sort = this.sort;
+        this.dataSourceToDisplay.paginator = this.paginator;
+        this.dataSourceToDisplay.paginator._intl.itemsPerPageLabel = 'Ilość rekordów na stronę';
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.dataSub.unsubscribe();
+  }
+
+  applyFilter($event: KeyboardEvent) {
+    const filterValue = ($event.target as HTMLInputElement).value;
+    this.dataSourceToDisplay.filter = filterValue.trim().toLowerCase();
+  }
+}
+
